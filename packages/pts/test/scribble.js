f6686660f6e93292f416a4f0f6c6d98b94dda4b4
iaforek <arkadiusz@jaworski.pl>
2021-07-19 16:53:28 +0200

    NO CHANGES. Formatting only

--- packages/pts/test/scribble.js
+++ packages/pts/test/scribble.js
@@ -1,25 +1,25 @@
+const assert = require("assert");
+const { PromiseTranformStream } = require("../lib/promise-transform-stream-ifca");
 
-const assert = require('assert');
-const { PromiseTranformStream } = require('../lib/promise-transform-stream-ifca')
-
-const gen = function*() {
+const gen = function* () {
     let i = 0;
-    while(i < 100) yield {a: i++};
-}
+    while (i < 100) yield { a: i++ };
+};
 
-const databaseSave = () => new Promise(res => setTimeout(res, 100));
+const databaseSave = () => new Promise((res) => setTimeout(res, 100));
 
 const str = new PromiseTranformStream(
-    {read: gen} // TODO: Add 
+    { read: gen } // TODO: Add
 )
-    .addTransform((x) => x.a % 2 ? x : undefined)
-    .addTransform((x) => ({b: x.a}))
+    .addTransform((x) => (x.a % 2 ? x : undefined))
+    .addTransform((x) => ({ b: x.a }))
     .addTransform((x) => {
         if (x.b % 10 === 7) throw new Error("This should be handled"); // 7 and 17 throws Error
 
         return x;
     })
-    .addHandler((err, x) => { // TODO: Add addHandler
+    .addHandler((err, x) => {
+        // TODO: Add addHandler
         if (x.b === 7 || x.b === 21) return undefined;
         throw err;
     })
@@ -27,14 +27,15 @@ const str = new PromiseTranformStream(
         (x) => {
             if (x.b === 17) throw new Error("This should be handled");
             if (x.b === 21) throw new Error("This should not be handled");
-        }, 
-        (err, x) => { if (x.b === 17) return x; throw err; } // TODO: add.
+        },
+        (err, x) => {
+            if (x.b === 17) return x;
+            throw err;
+        } // TODO: add.
         // Only applies to if (x.b === 17) throw new Error("This should be handled");
         // This drills down to IFCA to add such an option.
     )
-    .addTransform((x) => databaseSave(x))
-;
-
+    .addTransform((x) => databaseSave(x));
 (async () => {
     let items = [];
     try {
@@ -42,11 +43,9 @@ const str = new PromiseTranformStream(
             items.push(chunk.b);
         }
     } catch {
-        assert.deepStrictEqual(items, [1,3,5,9,11,13,15,17,19]); // 21 will go to the next catch
+        assert.deepStrictEqual(items, [1, 3, 5, 9, 11, 13, 15, 17, 19]); // 21 will go to the next catch
     }
-})().catch(
-    e => {
-        process.exitCode = 10;
-        console.error(e.stack);
-    }
-)
+})().catch((e) => {
+    process.exitCode = 10;
+    console.error(e.stack);
+});
