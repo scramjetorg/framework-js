08221117ec20ba0fa50d36d5eafe9264be65876f
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-14 15:46:53 +0000

    Add simpler version of scramjet use.

--- samples/csv/scramjet.js
+++ samples/csv/scramjet.js
@@ -13,4 +13,20 @@ StringStream
 	.stringify(record => Object.values( record ).join(','))
 	.append("\n")
 	.catch(err => `! Error occured ${err}`)
-	.pipe(fs.createWriteStream('vgsales-nintendo-sj.csv'));
+	.pipe(fs.createWriteStream('vgsales-nintendo-sj-1.csv'));
+
+// Simpler version
+// The result is the same but map is used insted of specialized functions(parse,stringfiy,append).
+StringStream
+	.from(fs.createReadStream('vgsales.csv', 'utf8'))
+	.setOptions({maxParallel: 100})
+	.lines()
+	.map(line => {
+		const [rank, name, platform, year, genre, publisher] = line.split(',');
+		return {rank, name, platform, year, genre, publisher};
+	})
+	.filter(record => record.publisher === 'Nintendo')
+	.map(record => Object.values( record ).join(','))
+	.map(record => record + '\n')
+	.catch(err => `! Error occured ${err}`)
+	.pipe(fs.createWriteStream('vgsales-nintendo-sj-2.csv'));
