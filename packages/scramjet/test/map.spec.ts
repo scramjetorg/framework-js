fa473582cd72f6b954fb2c3e88994f311f34c180
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-21 09:47:39 +0000

    Add '.map()' test applying multiple transforms at once

--- packages/scramjet/test/map.spec.ts
+++ packages/scramjet/test/map.spec.ts
@@ -44,3 +44,21 @@ test("DataStream can map chunks via async callback (to different type)", async (
 
     t.deepEqual(result, ['foo-1','foo-2','foo-3','foo-4','foo-5']);
 });
+
+test("DataStream can apply multiple map transforms", async (t) => {
+    const dsNumber = DataStream.from<number>([1,2,3,4,5]);
+
+    const result = await dsNumber
+        .map<number>(chunk => chunk*2)
+        .map<string>(chunk => {
+            return new Promise( res => {
+                setTimeout(() => {
+                    res(`${chunk}00`);
+                }, 10);
+            });
+        })
+        .map<number>(chunk => parseInt(chunk))
+        .toArray();
+
+    t.deepEqual(result, [200,400,600,800,1000]);
+});
