85cbb93ee078c6ccd9d1a48a9944c0ae8ae7901f
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-21 12:07:43 +0000

    Redefine base stream interface

--- packages/scramjet/lib/basestream.ts
+++ packages/scramjet/lib/basestream.ts
@@ -6,15 +6,15 @@ import { TransformFunction } from "../../ifca/lib/index";
 // * https://github.com/microsoft/TypeScript/issues/33892
 // * https://github.com/microsoft/TypeScript/issues/34516
 //
-// thus the workaround with BaseStreamCreator used below.
-
-class BaseStreamCreator {
-    static from<U>(input: Iterable<U> | AsyncIterable<U> | Readable): BaseStream<U> {
-        throw new Error('Not implemented!');
-    };
+// thus the workaround with BaseStream and BaseStreamCreators used below.
+export interface BaseStream<T> {
+    map<U>(callback: TransformFunction<T, U>): BaseStream<U>;
+    filter(callback: TransformFunction<T, Boolean>): BaseStream<T>;
 }
 
-export interface BaseStream<T> extends BaseStreamCreator {
-    map<U>(callback: TransformFunction<T,U>): BaseStream<U>;
-    filter(callback: TransformFunction<T,Boolean>): BaseStream<T>;
+export class BaseStreamCreators {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    static from<U>(input: Iterable<U> | AsyncIterable<U> | Readable): BaseStream<U> {
+        throw new Error("Not implemented!");
+    }
 }
