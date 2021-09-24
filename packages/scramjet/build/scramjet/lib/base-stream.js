5abb6db0a077435df737d1fea87e219a4b43dbe1
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-24 11:48:11 +0000

    Remove and ignore build dir

--- packages/scramjet/build/scramjet/lib/base-stream.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseStreamCreators = void 0;
-class BaseStreamCreators {
-    static from(input) {
-        throw new Error("Not implemented!");
-    }
-}
-exports.BaseStreamCreators = BaseStreamCreators;
--- packages/scramjet/build/scramjet/test/filter.spec.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ava_1 = require("ava");
-(0, ava_1.default)("DataStream can filter chunks via sync callback", async (t) => {
-    t.pass();
-});
