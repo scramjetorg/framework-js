ced4bdaf55b080ba0120bc668009e1dd4ac287ef
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-28 09:10:26 +0000

    Remove unnecessary comment

--- packages/scramjet/test/creation.spec.ts
+++ packages/scramjet/test/creation.spec.ts
@@ -13,7 +13,7 @@ test("DataStream can be constructed", (t) => {
 test("DataStream can be created via static from method", (t) => {
     const dsNumber = DataStream.from<number>([1,2,3,4]);
     const dsString = DataStream.from<string>(['1','2','3','4']);
-    const dsAny = DataStream.from([1,2,'3','4']); // Not sure how and if we want to force using generics.
+    const dsAny = DataStream.from([1,2,'3','4']);
 
     t.true(dsNumber instanceof DataStream);
     t.true(dsString instanceof DataStream);
