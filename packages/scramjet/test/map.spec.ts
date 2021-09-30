642d028d3543d0ca7cb4417ce87ef3cba26c1d9f
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-30 09:34:42 +0000

    Introduce tests for variadic args, fix tests async calls

--- packages/scramjet/test/map.spec.ts
+++ packages/scramjet/test/map.spec.ts
@@ -2,63 +2,74 @@ import test from "ava";
 import { DataStream } from "../lib/data-stream";
 
 test("DataStream can map chunks via sync callback (to same type)", async (t) => {
-    const dsNumber = DataStream.from<number>([1,2,3,4,5]);
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5]);
+    const result = await dsNumber.map<number>(chunk => chunk * 2).toArray();
 
-    const result = await dsNumber.map<number>(chunk => chunk*2).toArray();
-
-    t.deepEqual(result, [2,4,6,8,10]);
+    t.deepEqual(result, [2, 4, 6, 8, 10]);
 });
 
 test("DataStream can map chunks via sync callback (to different type)", async (t) => {
-    const dsNumber = DataStream.from<number>([1,2,3,4,5]);
-
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5]);
     const result = await dsNumber.map<string>(chunk => `foo-${chunk}`).toArray();
 
-    t.deepEqual(result, ['foo-1','foo-2','foo-3','foo-4','foo-5']);
+    t.deepEqual(result, ["foo-1", "foo-2", "foo-3", "foo-4", "foo-5"]);
 });
 
 test("DataStream can map chunks via async callback (to same type)", async (t) => {
-    const dsNumber = DataStream.from<number>([1,2,3,4,5]);
-
-    const result = await dsNumber.map<number>(chunk => {
-        return new Promise( res => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5]);
+    const result = await dsNumber.map<number>(async chunk => {
+        return new Promise(res => {
             setTimeout(() => {
-                res(chunk*2);
+                res(chunk * 2);
             }, 10);
         });
     }).toArray();
 
-    t.deepEqual(result, [2,4,6,8,10]);
+    t.deepEqual(result, [2, 4, 6, 8, 10]);
 });
 
 test("DataStream can map chunks via async callback (to different type)", async (t) => {
-    const dsNumber = DataStream.from<number>([1,2,3,4,5]);
-
-    const result = await dsNumber.map<string>(chunk => {
-        return new Promise( res => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5]);
+    const result = await dsNumber.map<string>(async chunk => {
+        return new Promise(res => {
             setTimeout(() => {
                 res(`foo-${chunk}`);
             }, 10);
         });
     }).toArray();
 
-    t.deepEqual(result, ['foo-1','foo-2','foo-3','foo-4','foo-5']);
+    t.deepEqual(result, ["foo-1", "foo-2", "foo-3", "foo-4", "foo-5"]);
 });
 
 test("DataStream can apply multiple map transforms", async (t) => {
-    const dsNumber = DataStream.from<number>([1,2,3,4,5]);
-
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5]);
     const result = await dsNumber
-        .map<number>(chunk => chunk*2)
-        .map<string>(chunk => {
-            return new Promise( res => {
+        .map<number>(chunk => chunk * 2)
+        .map<string>(async chunk => {
+            return new Promise(res => {
                 setTimeout(() => {
                     res(`${chunk}00`);
                 }, 10);
             });
         })
-        .map<number>(chunk => parseInt(chunk))
+        .map<number>(chunk => parseInt(chunk, 10))
         .toArray();
 
-    t.deepEqual(result, [200,400,600,800,1000]);
+    t.deepEqual(result, [200, 400, 600, 800, 1000]);
+});
+
+test("DataStream map passes variadic args", async (t) => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5]);
+    const result = await dsNumber
+        .map<number>((chunk, multiplier: number) => chunk * multiplier, 3)
+        .map<string>(async (chunk, postfix: string) => {
+            return new Promise(res => {
+                setTimeout(() => {
+                    res(`${chunk}${postfix}`);
+                }, 10);
+            });
+        }, "00")
+        .toArray();
+
+    t.deepEqual(result, ["300", "600", "900", "1200", "1500"]);
 });
