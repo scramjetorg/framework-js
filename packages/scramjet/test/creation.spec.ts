9d706909e481acc8005efd95bee500d9f8d925a2
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-09-30 10:42:10 +0000

    Reformat tests

--- packages/scramjet/test/creation.spec.ts
+++ packages/scramjet/test/creation.spec.ts
@@ -11,9 +11,9 @@ test("DataStream can be constructed", (t) => {
 });
 
 test("DataStream can be created via static from method", (t) => {
-    const dsNumber = DataStream.from<number>([1,2,3,4]);
-    const dsString = DataStream.from<string>(['1','2','3','4']);
-    const dsAny = DataStream.from([1,2,'3','4']);
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4]);
+    const dsString = DataStream.from<string>(["1", "2", "3", "4"]);
+    const dsAny = DataStream.from([1, 2, "3", "4"]);
 
     t.true(dsNumber instanceof DataStream);
     t.true(dsString instanceof DataStream);
@@ -23,35 +23,35 @@ test("DataStream can be created via static from method", (t) => {
 test("DataStream can be cretaed from an empty iterable", async (t) => {
     const input: number[] = [];
     const dsNumber = DataStream.from<number>(input);
-
     const result = await dsNumber.toArray();
+
     t.deepEqual(result, input);
 });
 
 test("DataStream can read from iterable", async (t) => {
-    const input = [1,2,3,4,5,6,7,8];
+    const input = [1, 2, 3, 4, 5, 6, 7, 8];
     const dsNumber = DataStream.from<number>(input);
-
     const result = await dsNumber.toArray();
+
     t.deepEqual(result, input);
 });
 
 test("DataStream can read from generator (iterable)", async (t) => {
-    function * numbers() {
-        for(let i = 0; i < 8; i++) {
+    function* numbers() {
+        for (let i = 0; i < 8; i++) {
             yield i;
         }
     }
 
     const dsNumber = DataStream.from<number>(numbers());
-
     const result = await dsNumber.toArray();
-    t.deepEqual(result, [0,1,2,3,4,5,6,7]);
+
+    t.deepEqual(result, [0, 1, 2, 3, 4, 5, 6, 7]);
 });
 
 test("DataStream can read from async iterable", async (t) => {
     const words = {
-        async* [Symbol.asyncIterator]() {
+        async*[Symbol.asyncIterator]() {
             yield "foo";
             yield "bar";
             yield "baz";
@@ -62,10 +62,10 @@ test("DataStream can read from async iterable", async (t) => {
             yield "bax2";
         }
     };
-
     const dsWords = DataStream.from<string>(words);
     const result = await dsWords.toArray();
-    t.deepEqual(result, ['foo','bar','baz','bax','foo2','bar2','baz2','bax2']);
+
+    t.deepEqual(result, ["foo", "bar", "baz", "bax", "foo2", "bar2", "baz2", "bax2"]);
 });
 
 // test("DataStream can read from async generator (async iterable)", async (t) => {}); // TBD
@@ -73,19 +73,18 @@ test("DataStream can read from async iterable", async (t) => {
 // test("DataStream can read from another scramjet stream", async (t) => {}); // TBD
 
 test("DataStream can read from readable", async (t) => {
-    const readable = createReadStream('./test/helpers/sample.txt', 'utf8');
-
+    const readable = createReadStream("./test/helpers/sample.txt", "utf8");
     const dsString = DataStream.from<string>(readable);
-
     const result = await dsString.toArray();
-    t.deepEqual(result, ['foo\nbar\nbaz\nbax\n']);
+
+    t.deepEqual(result, ["foo\nbar\nbaz\nbax\n"]);
 });
 
 test("DataStream will not start reading until 'output' transfomration is called (generator)", async (t) => {
     let startedReading = false;
 
-    function * numbers() {
-        for(let i = 0; i < 8; i++) {
+    function* numbers() {
+        for (let i = 0; i < 8; i++) {
             startedReading = true;
             yield i;
         }
@@ -101,14 +100,13 @@ test("DataStream will not start reading until 'output' transfomration is called 
 });
 
 test("DataStream will not start reading until 'output' transfomration is called (readable)", async (t) => {
-    const readable = createReadStream('./test/helpers/sample.txt', 'utf8');
-
+    const readable = createReadStream("./test/helpers/sample.txt", "utf8");
     const dsString = DataStream.from<string>(readable);
 
-    // Since readable will be read at once, if it's not ended means reading haven't started yet.
+    // Since readable will be read at once, if it"s not ended means reading haven"t started yet.
     t.false(readable.readableEnded);
 
     await dsString.toArray();
 
-    t.true(readable.readableEnded)
+    t.true(readable.readableEnded);
 });
