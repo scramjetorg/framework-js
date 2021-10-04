249ebe83d2a41365a60c1dd6743863b410a77d90
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-10-04 11:36:40 +0000

    Fix tests

--- packages/scramjet/test/filter.spec.ts
+++ packages/scramjet/test/filter.spec.ts
@@ -3,7 +3,7 @@ import { DataStream } from "../lib/data-stream";
 
 test("DataStream can filter chunks via sync callback", async (t) => {
     const dsNumber = DataStream.from<number>([1, 2, 3, 4, 5, 6, 7]);
-    const result = await dsNumber.filter(chunk => !!(chunk%2)).toArray();
+    const result = await dsNumber.filter(chunk => !!(chunk % 2)).toArray();
 
     t.deepEqual(result, [1, 3, 5, 7]);
 });
@@ -41,8 +41,8 @@ test("DataStream can apply multiple filter transforms", async (t) => {
 test("DataStream passes variadic args to filter", async (t) => {
     const dsString = DataStream.from<string>(["1", "2", "3", "4", "10", "20", "30", "40", "100", "200", "300", "400"]);
     const result = await dsString
-        .filter((chunk, limiter: number) => chunk.length < limiter, 3)
-        .filter(async (chunk, prefix: string, prefixes: string[]) => {
+        .filter((chunk, limiter) => chunk.length < limiter, 3)
+        .filter(async (chunk, prefix, prefixes) => {
             return new Promise(res => {
                 setTimeout(() => {
                     res(!!(chunk.startsWith(prefix) || chunk.startsWith(prefixes[0]) || chunk.startsWith(prefixes[1])));
