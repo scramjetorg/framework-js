b53ada28debd5dfde34598d649506eb36e6d99cd
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-10-11 13:49:10 +0000

    Move source code from packages to src dir

---
+++ src/streams/base-stream.ts
@@ -0,0 +1,21 @@
+import { Readable } from "stream";
+import { TransformFunction } from "../ifca";
+
+// There is no support for static methods in interfaces (or abstract classes) in TS, see:
+//
+// * https://github.com/microsoft/TypeScript/issues/33892
+// * https://github.com/microsoft/TypeScript/issues/34516
+//
+// thus the workaround with BaseStream and BaseStreamCreators used below.
+
+export interface BaseStream<T extends any> {
+    map<U>(callback: TransformFunction<T, U>): BaseStream<U>;
+    filter(callback: TransformFunction<T, Boolean>): BaseStream<T>;
+}
+
+export abstract class BaseStreamCreators {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    static from<T extends any>(input: Iterable<T> | AsyncIterable<T> | Readable): BaseStream<T> {
+        throw new Error("Not implemented!");
+    }
+}
---
+++ test/streams/creation.spec.ts
@@ -0,0 +1,160 @@
+import test from "ava";
+import { createReadStream } from "fs";
+import { defer } from "../../ifca/utils";
+import { DataStream } from "../lib/data-stream";
+
+test("DataStream can be constructed", (t) => {
+    const dsNumber = new DataStream<number>();
+    const dsString = new DataStream<string>();
+
+    t.true(dsNumber instanceof DataStream);
+    t.true(dsString instanceof DataStream);
+});
+
+test("DataStream can be created via static from method", (t) => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4]);
+    const dsString = DataStream.from<string>(["1", "2", "3", "4"]);
+    const dsAny = DataStream.from([1, 2, "3", "4"]);
+
+    t.true(dsNumber instanceof DataStream);
+    t.true(dsString instanceof DataStream);
+    t.true(dsAny instanceof DataStream);
+});
+
+test("DataStream can be cretaed from an empty iterable", async (t) => {
+    const input: number[] = [];
+    const dsNumber = DataStream.from<number>(input);
+    const result = await dsNumber.toArray();
+
+    t.deepEqual(result, input);
+});
+
+test("DataStream can read from iterable", async (t) => {
+    const input = [1, 2, 3, 4, 5, 6, 7, 8];
+    const dsNumber = DataStream.from<number>(input);
+    const result = await dsNumber.toArray();
+
+    t.deepEqual(result, input);
+});
+
+test("DataStream can read from generator (iterable)", async (t) => {
+    function* numbers() {
+        for (let i = 0; i < 8; i++) {
+            yield i;
+        }
+    }
+
+    const dsNumber = DataStream.from<number>(numbers());
+    const result = await dsNumber.toArray();
+
+    t.deepEqual(result, [0, 1, 2, 3, 4, 5, 6, 7]);
+});
+
+test("DataStream can read from async iterable", async (t) => {
+    const words = {
+        async*[Symbol.asyncIterator]() {
+            yield "foo";
+            yield "bar";
+            yield "baz";
+            yield "bax";
+            yield "foo2";
+            yield "bar2";
+            yield "baz2";
+            yield "bax2";
+        }
+    };
+    const dsWords = DataStream.from<string>(words);
+    const result = await dsWords.toArray();
+
+    t.deepEqual(result, ["foo", "bar", "baz", "bax", "foo2", "bar2", "baz2", "bax2"]);
+});
+
+test("DataStream can read from another scramjet stream", async (t) => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4]);
+    const dsNumber2 = DataStream.from<number>(dsNumber);
+    const result = await dsNumber2.toArray();
+
+    t.deepEqual(result, [1, 2, 3, 4]);
+});
+
+test("DataStream can read from readable", async (t) => {
+    const readable = createReadStream("./test/helpers/sample.txt", "utf8");
+    const dsString = DataStream.from<string>(readable);
+    const result = await dsString.toArray();
+
+    t.deepEqual(result, ["foo\nbar\nbaz\nbax\n"]);
+});
+
+test("DataStream will not start reading until 'output' transfomration is called (generator)", async (t) => {
+    let startedReading = false;
+
+    function* numbers() {
+        for (let i = 0; i < 8; i++) {
+            startedReading = true;
+            yield i;
+        }
+    }
+
+    const dsNumber = DataStream.from<number>(numbers());
+
+    t.false(startedReading);
+
+    await dsNumber.toArray();
+
+    t.true(startedReading);
+});
+
+test("DataStream will not start reading until 'output' transfomration is called (readable)", async (t) => {
+    const readable = createReadStream("./test/helpers/sample.txt", "utf8");
+    const dsString = DataStream.from<string>(readable);
+
+    // Since readable will be read at once, if it's not ended means reading haven't started yet.
+    t.false(readable.readableEnded);
+
+    await dsString.toArray();
+
+    t.true(readable.readableEnded);
+});
+
+test("DataStream can be corked and uncorked", async (t) => {
+    const ref: any = {};
+    const yielded: number[] = [];
+
+    function* numbers() {
+        for (let i = 0; i < 8; i++) {
+            yield i;
+            yielded.push(i);
+            if (i % 3 === 1) {
+                ref.dataStream._cork();
+            }
+        }
+    }
+
+    const dsNumber = DataStream.from<number>(numbers());
+
+    ref.dataStream = dsNumber as any;
+
+    t.deepEqual(yielded, []);
+
+    const resultPromise = dsNumber.toArray();
+
+    await defer(0);
+
+    t.deepEqual(yielded, [0, 1]);
+
+    dsNumber._uncork();
+
+    await defer(0);
+
+    t.deepEqual(yielded, [0, 1, 2, 3, 4]);
+
+    dsNumber._uncork();
+
+    await defer(0);
+
+    t.deepEqual(yielded, [0, 1, 2, 3, 4, 5, 6, 7]);
+
+    const result = await resultPromise;
+
+    t.deepEqual(result, [0, 1, 2, 3, 4, 5, 6, 7]);
+});
