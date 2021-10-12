4f1974675b2836b805115e716bdf56037022e80c
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-10-12 06:57:01 +0000

    Fix lint errors in tests

--- test/ifca/oal.spec.ts
+++ test/ifca/oal.spec.ts
@@ -1,6 +1,6 @@
 import test from "ava";
 import { IFCA } from "../../src/ifca";
-import { defer } from "../../src/utils"
+import { defer } from "../../src/utils";
 
 /**
  * How many items can be waiting to be flushed
@@ -11,14 +11,15 @@ test("Acceptable latency test", async (t) => {
     let sum: bigint = BigInt(0);
     let cnt = BigInt(0);
 
-    const ifca = new IFCA(MAX_PARALLEL, ({i}: {i: number}) => ({i, ts: process.hrtime.bigint()}))
-        .addTransform(({i, ts}) => ({ i, latency: process.hrtime.bigint() - ts }))
+    const ifca = new IFCA(MAX_PARALLEL, ({ i }: {i: number}) => ({ i, ts: process.hrtime.bigint() }))
+        .addTransform(({ i, ts }) => ({ i, latency: process.hrtime.bigint() - ts }))
     ;
 
     await Promise.all([
         (async () => {
             for (let i = 0; i < 4000; i++) {
-                const ret = ifca.write({i: i+1});
+                const ret = ifca.write({ i: i + 1 });
+
                 await Promise.all([ret, defer(1)]);
             }
             ifca.end(); // TODO: test for correct end operation
@@ -26,13 +27,15 @@ test("Acceptable latency test", async (t) => {
         (async () => {
             await defer(10);
             let i = 0;
-            while(++i) {
+
+            while (++i) {
                 const data = await ifca.read();
+
                 if (data === null) {
-                    t.log("data done")
+                    t.log("data done");
                     return;
                 }
-                t.is(data?.i, i, `Data is in correct order`);
+                t.is(data?.i, i, "Data is in correct order");
                 if (i < 2000) continue;
 
                 cnt++;
@@ -41,7 +44,8 @@ test("Acceptable latency test", async (t) => {
         })().finally(() => t.log("Read done"))
     ]);
 
-    const latency = Number(sum * BigInt(1e6) / cnt ) / 1e6;
+    const latency = Number(sum * BigInt(1e6) / cnt) / 1e6;
+
     t.log("Latency:", latency);
     t.false(latency > 1e4, "Latency does not exceed 10us");
 });
