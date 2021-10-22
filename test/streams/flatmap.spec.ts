dc9ad380395f31e4a3b8a088a89246e53fe49461
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-10-22 11:26:37 +0000

    Implement 'DataStream.flatMap'
    This commit introduces '.flatMap()' method implementation which is
    consistent with JavaScript 'Array.prototype.flatMap()'.
    
    Since 'flatMap' is internally 'map -> filter (empty results) -> flatten'
    I have reused '.map()' and '.filter()` methods and only added the
    missing part which is writing flattened chunks to new DataStream
    instance which is then returned. This is done through async generator
    which ensures newly created data stream will read new chunks only when
    it can keeping back pressure intact.

---
+++ test/streams/flatmap.spec.ts
@@ -0,0 +1,51 @@
+import test from "ava";
+import { DataStream } from "../../src/streams/data-stream";
+
+test("DataStream can flat-map chunks via sync callback (to same type)", async (t) => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4]);
+    const result = await dsNumber.flatMap<number>(chunk => [chunk * 2]).toArray();
+
+    t.deepEqual(result, [2, 4, 6, 8]);
+});
+
+test("DataStream can flat-map chunks via async callback (to same type)", async (t) => {
+    const dsString = DataStream.from<string>(["it's Sunny in", "", "California"]);
+    const result = await dsString.flatMap<string>(chunk => chunk.split(" ")).toArray();
+
+    t.deepEqual(result, ["it's", "Sunny", "in", "", "California"]);
+});
+
+test("DataStream flatMap flattens only one level", async (t) => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4]);
+    const result = await dsNumber.flatMap<number[]>(chunk => [[chunk * 2]]).toArray();
+
+    t.deepEqual(result, [[2], [4], [6], [8]]);
+});
+
+test("DataStream flatMap filters and duplicates chunks correctly", async (t) => {
+    const dsNumber = DataStream.from<number>([5, 4, -3, 20, 17, -33, -4, 18]);
+    const result = await dsNumber.flatMap<number>(chunk => {
+        if (chunk < 0) {
+            return [];
+        }
+        return chunk % 2 === 0 ? [chunk] : [chunk - 1, 1];
+    }).toArray();
+
+    t.deepEqual(result, [4, 1, 4, 20, 16, 1, 18]);
+});
+
+test("DataStream flatMap passes typed variadic args", async (t) => {
+    const dsNumber = DataStream.from<number>([1, 2, 3, 4]);
+    const result = await dsNumber
+        .flatMap<number, number[]>((chunk, multiplier) => [chunk * multiplier], 3)
+        .flatMap<string, string[]>(async (chunk, postfix, text) => {
+            return new Promise(res => {
+                setTimeout(() => {
+                    res([`${chunk}${postfix}`, text]);
+                }, 10);
+            });
+        }, "00", "foo")
+        .toArray();
+
+    t.deepEqual(result, ["300", "foo", "600", "foo", "900", "foo", "1200", "foo"]);
+});
