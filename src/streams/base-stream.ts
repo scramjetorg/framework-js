873672d6f107148804f476d34c054abf08167d4c
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-10-28 08:07:59 +0000

    Fix 'BaseStream.flatMap' declaration

--- src/streams/base-stream.ts
+++ src/streams/base-stream.ts
@@ -1,6 +1,6 @@
-import { TransformFunction } from "../types";
+import { TransformFunction, AnyIterable } from "../types";
 export interface BaseStream<T extends any> {
     map<U, W extends any[]>(callback: TransformFunction<T, U, W>, ...args: W): BaseStream<U>;
-    flatMap<U, W extends any[]>(callback: TransformFunction<T, U[], W>, ...args: W): BaseStream<U>;
+    flatMap<U, W extends any[]>(callback: TransformFunction<T, AnyIterable<U>, W>, ...args: W): BaseStream<U>;
     filter<W extends any[]>(callback: TransformFunction<T, Boolean, W>, ...args: W): BaseStream<T>;
 }
