394b2fa824d759e05a85e127cae0fd7ec77d8e61
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-10-28 10:37:48 +0000

    Introduce 'DataStream.reduce' method

--- src/streams/base-stream.ts
+++ src/streams/base-stream.ts
@@ -3,4 +3,5 @@ export interface BaseStream<T extends any> {
     map<U, W extends any[]>(callback: TransformFunction<T, U, W>, ...args: W): BaseStream<U>;
     flatMap<U, W extends any[]>(callback: TransformFunction<T, AnyIterable<U>, W>, ...args: W): BaseStream<U>;
     filter<W extends any[]>(callback: TransformFunction<T, Boolean, W>, ...args: W): BaseStream<T>;
+    reduce<U = T>(callback: (previousValue: U, currentChunk: T) => U, initial?: U): Promise<U>;
 }
