eb28e588c45683dc6433bfc98290ebc31f7534d7
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-11-03 08:31:26 +0000

    Fix static and runtime return type for StringSplit for inherited methods
    This commit is trying to solve the issue of "polymorphic generic this
    as a return type".
    
    For more in-depth description see:
    https://github.com/scramjetorg/scramjet-dev/pull/28.
    
    This issue is related to a fact that in `DataStream<T>` class we have
    multiple methods (transfroms) which returns `DataStream<U>` (so the
    instance of the same class, but with different type parameter). This
    works fine for `DataStream` itself but causes problems for derived
    classes as a return type is not dynamic and inhertied methods called
    on an instance of derived class will always return the instance
    of a parent class.
    
    First change was to introduce `.create()` method which is used inside
    tramsforms directly instead of calling `new DataStream...`. This allows
    derived classes to override it and have control over how its instances
    are created. With `.create()` in use, new instance based on call context
    (current this) is created. This results with a correct return type
    during runtime.
    
    The second change was to override transforms signatures in the derived
    class so correct return type could be declared. Those overriden
    transforms call `super.transform(...)` internally so the logic stays
    exactly the same. This solves the issue of TS not correctly recognizing
    return type (as without it, method signatures declare base class
    as return type).
    
    Above works well for generic derived classes. However, for non-generic
    derived classes (like `StringStream` in our case), overridden transform
    signatures (and also `.create()` method) needs to be non-generic as well
    to work with this approach. And so the third change was to overload
    transforms in a base class to have both non-generic and generic versions.

--- test/streams/string/creation.spec.ts
+++ test/streams/string/creation.spec.ts
@@ -17,12 +17,51 @@ test("StringStream split returns instance of StringStream", (t) => {
     const stringStream = StringStream.from(["1", "2", "3", "4"]);
     const newStream = stringStream.split("2");
 
-    console.log("split", newStream); // returns DataStream due to .asNewFlattenedStream call
-    // newStream.split("3"); // static code analysis accpets that but it throws
-    // during runtime since newStream is DataStream
+    console.log("split", newStream);
 
     t.true(newStream instanceof StringStream, `Should be an instance of StringStream, not ${newStream.constructor.name}`);
     t.deepEqual(newStream.constructor.name, "StringStream");
+
+    const newStream2 = newStream.split("1");
+
+    console.log("split", newStream2);
+
+    t.true(newStream2 instanceof StringStream, `Should be an instance of StringStream, not ${newStream2.constructor.name}`);
+    t.deepEqual(newStream2.constructor.name, "StringStream");
+});
+
+test("StringStream flatMap returns instance of StringStream", (t) => {
+    const stringStream = StringStream.from(["1", "2", "3", "4"]);
+    const newStream = stringStream.flatMap(chunk => [chunk]);
+
+    console.log("flatMap", newStream);
+
+    t.true(newStream instanceof StringStream, `Should be an instance of StringStream, not ${newStream.constructor.name}`);
+    t.deepEqual(newStream.constructor.name, "StringStream");
+
+    const newStream2 = newStream.split("1");
+
+    console.log("split", newStream2);
+
+    t.true(newStream2 instanceof StringStream, `Should be an instance of StringStream, not ${newStream2.constructor.name}`);
+    t.deepEqual(newStream2.constructor.name, "StringStream");
+});
+
+test("StringStream filter returns instance of StringStream", (t) => {
+    const stringStream = StringStream.from(["1", "2", "3", "4"]);
+    const newStream = stringStream.filter(chunk => parseInt(chunk, 10) % 2 === 0);
+
+    console.log("filter", newStream);
+
+    t.true(newStream instanceof StringStream, `Should be an instance of StringStream, not ${newStream.constructor.name}`);
+    t.deepEqual(newStream.constructor.name, "StringStream");
+
+    const newStream2 = newStream.split("1");
+
+    console.log("split", newStream2);
+
+    t.true(newStream2 instanceof StringStream, `Should be an instance of StringStream, not ${newStream2.constructor.name}`);
+    t.deepEqual(newStream2.constructor.name, "StringStream");
 });
 
 test("StringStream map returns instance of StringStream", (t) => {
@@ -32,43 +71,6 @@ test("StringStream map returns instance of StringStream", (t) => {
     console.log("map", newStream); // returns StringStream (since it returns this internally)
     // newStream.split("3"); // static code analysis does not accept that since
     // map should return DataStream (but it works at runtime)
-
-    t.true(newStream instanceof StringStream, `Should be an instance of StringStream, not ${newStream.constructor.name}`);
-    t.deepEqual(newStream.constructor.name, "StringStream");
-});
-
-test("StringStream flatMap returns instance of StringStream", (t) => {
-    const stringStream = StringStream.from(["1", "2", "3", "4"]);
-    const newStream = stringStream.flatMap(chunk => [chunk]);
-
-    console.log("flatMap", newStream); // returns DataStream due to .asNewFlattenedStream call
-    // newStream.split("3"); // static code analysis does not accept that since
-    // map should return DataStream (an it throws at runtime)
-
-    t.true(newStream instanceof StringStream, `Should be an instance of StringStream, not ${newStream.constructor.name}`);
-    t.deepEqual(newStream.constructor.name, "StringStream");
-});
-
-test("StringStream filter returns instance of StringStream", (t) => {
-    const stringStream = StringStream.from(["1", "2", "3", "4"]);
-    const newStream = stringStream.filter(chunk => parseInt(chunk, 10) % 2 === 0);
-
-    console.log("filter", newStream); // returns StringStream (since it returns this internally)
-    // newStream.split("3"); // static code analysis does not accept that since
-    // map should return DataStream (but it works at runtime)
-
-    // This is kind of tricky since static code analysis shows that "stringStream.filter" returns
-    // DataString<string> (as declared in method return type)
-    // but on runtime, it internally returns "this" which is StringStream
-    //
-    // So it can be solved like:
-    //
-    // StringStream.filter<W extends any[] = []>(
-    //     callback: TransformFunction<string, Boolean, W>,
-    //     ...args: W): StringStream
-    // {
-    //     return super.filter(callback, ...args) as StringStream;
-    // }
 
     t.true(newStream instanceof StringStream, `Should be an instance of StringStream, not ${newStream.constructor.name}`);
     t.deepEqual(newStream.constructor.name, "StringStream");
