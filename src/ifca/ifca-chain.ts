409da518b8dc58c7e2e10a5ebc6b314879eea4cd
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-11-22 09:07:03 +0000

    Introduce 'IFCAChain' class
    The 'IFCAChain' class is an additional layer between stream instances
    and IFCA instances. It allows for easier operating on IFCA instances
    by related stream instances and also sharing such IFCA instances
    between multiple stream instances.
    
    It was introduced to allow writing to any intermediatte stream instance
    created during transforms and to allow chaining and sharing IFCAs
    between such instances.

---
+++ src/ifca/ifca-chain.ts
@@ -0,0 +1,60 @@
+import { IFCA } from "../ifca";
+import { MaybePromise, IFCAOptions } from "../types";
+
+export class IFCAChain<IN, OUT> {
+    /**
+     * All IFCA instances managed by this chain.
+     */
+    private chain: Array<IFCA<any, any>> = [];
+
+    /**
+     * Creates and adds new IFCA to this chain,
+     *
+     * @param {IFCAOptions} options IFCA options.
+     * @returns {IFCA} Newly created IFCA instance.
+     */
+    add<NEW_IN, NEW_OUT>(options: IFCAOptions): IFCA<NEW_IN, NEW_OUT, any> {
+        const ifca = new IFCA<NEW_IN, NEW_OUT, any>(options);
+
+        this.chain.push(ifca);
+
+        return ifca;
+    }
+
+    /**
+     * Gets last IFCA instance from this chain.
+     *
+     * @returns {IFCA} IFCA instance.
+     */
+    get<NEW_IN, NEW_OUT>(): IFCA<NEW_IN, NEW_OUT, any> {
+        return this.chain[this.chain.length - 1];
+    }
+
+    /**
+     * Writes to IFCA chain (to first IFCA in a chain).
+     *
+     * @param {IN} chunk Chunk to be written.
+     * @returns {MaybePromise<void>} Drain value/promise.
+     */
+    write(chunk: IN): MaybePromise<void> {
+        return this.chain[0].write(chunk);
+    }
+
+    /**
+     * Reads from IFCA chain (from last IFCA in a chain).
+     *
+     * @returns {MaybePromise<OUT|null>} Promise resolving to a chunk, chunk itself or null if there is nothing to read.
+     */
+    read(): MaybePromise<OUT|null> {
+        return this.chain[this.chain.length - 1].read();
+    }
+
+    /**
+     * Ends IFCA chain.
+     *
+     * @returns {MaybePromise<void>} Promise resolving (or already resolved) when chain is ended.
+     */
+    end(): MaybePromise<void> {
+        return this.chain[0].end();
+    }
+}
