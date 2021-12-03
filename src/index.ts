c52ed7440f8694cad8728e3a23c6f67467dac10a
Krzysztof Krztoń <krzysztof.krzton@scramjet.org>
2021-12-03 11:30:23 +0000

    Add entry point file

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { BaseStream } from "./streams/base-stream";
+import { DataStream } from "./streams/data-stream";
+import { StringStream } from "./streams/string-stream";
+
+export {
+    BaseStream,
+    DataStream,
+    StringStream
+};
